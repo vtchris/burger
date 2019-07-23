935908f35299adb38049ed935f469d0db4f2d6e6
VTC <vtchris@hotmail.com>
2019-07-23 13:46:28 -0400

    attempt to add db to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 var keys = require("./keys");
 var mysql = require('mysql');
 
+var connection;
 // MySql connection string
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: keys.mysql.id,
-    password: keys.mysql.p,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: keys.mysql.id,
+        password: keys.mysql.p,
+        database: "burgers_db"
+    });
+}
 // Test database connection
 connection.connect(function (err) {
     if (err) throw err;
